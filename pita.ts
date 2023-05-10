609bd05d5cce01104e60b8c157c2aa5f79ca1716
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-05-10 11:17:20 +0200

    chore(cli): :bookmark: update cli to version 0.2.0

--- pita.ts
+++ pita.ts
@@ -23,7 +23,7 @@ if (import.meta.main) {
 
 	const main = new Command()
 		.name('pita')
-		.version('0.1.5')
+		.version('0.2.0')
 		.meta('deno', Deno.version.deno)
 		.description('🫓 Project manager for RedPitaya web apps 🫓')
 		.command('requirements', requirements)
